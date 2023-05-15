99d366928eeba2948211ed8a8901f489ae71e7cb
ray-robot <hello@raynetwork.io>
2023-05-15 11:44:00 +0300

    Fixing top-level dereference non-assignment issue

--- codegen/_prepare.js
+++ codegen/_prepare.js
@@ -2,9 +2,12 @@ const fs = require("fs")
 const YAML = require("yaml")
 const { dereferenceSync } = require("dereference-json-schema")
 
-const schemaYaml = fs.readFileSync("codegen/koiosapi.yaml", "utf8")
-const schemaPaths = dereferenceSync(YAML.parse(schemaYaml)).paths
+const schemaPaths = dereferenceSync(YAML.parse(fs.readFileSync("codegen/koiosapi.yaml", "utf8"))).paths
+
 delete schemaPaths["/submittx"] // removing the submit tx method because it is not represented in koios-lite by default
+schemaPaths["/credential_txs"].post.responses["200"].content["application/json"].schema =
+  schemaPaths["/address_txs"].post.responses["200"].content["application/json"].schema // weird hack to fix $ref top-level non-assigment problem on /credential_txs path, need to write own parser?
+
 const schemaJson = Object.entries(schemaPaths).flatMap(([path, pathObject]) => {
   return ["get", "post"]
     .map((method) => ({ path, method, operation: pathObject?.[method] }))
