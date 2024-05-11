bd20d0c906532d221a762f1e0faf5eb135fc9930
xray-robot <robot@xray.app>
2024-05-11 20:59:29 +0300

    Export schema types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import createClient, { HeadersOptions } from "openapi-fetch"
 import type { paths } from "./schema/schema"
+import * as KoiosTypes from "./schema/schema"
 
 export default (baseUrl: string, headers?: HeadersOptions) => {
   return createClient<paths>({
